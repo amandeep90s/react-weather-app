26c2af843d20ee15ace5d891ef0f6398724d07ff
Amandeep Singh <96813271+amandeep-udaan@users.noreply.github.com>
2022-08-12 21:55:05 +0530

    Final code

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9f0f2fba1emsh7e979a0de640f80p1629dfjsnd48faf9eb7fd",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "c753d04c02fa7989f550ff8372e4d409";
